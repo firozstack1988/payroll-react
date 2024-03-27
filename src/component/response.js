66c75874ea908e9c306ef0d98fc20ae192260315
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2024-03-27 12:02:37 +0600

    Add files via upload

---
+++ src/component/response.js
@@ -0,0 +1,19 @@
+const responseData={
+    success:"success",
+    PASSWORD_MISMATCH:"Password Not Matching",
+    content:"",
+    USER_ERROR:"User not found",
+    logout:"Successfully Logout",
+    PASSWORD_SUCCESS_MSG:"Password changed successfully",
+    PASSWORD_FAILURE_MSG:"Password changed failure",
+    NEWPASSWORD_CONFIRMPASSWORD:"New password and confirm password must be same",
+    INSERT_SUCCESS:"Successfully inserted",
+    INSERT_FAILURE:"Record not inserted",
+    NOT_FOUND:"No data found",
+    DATA_FOUND:"Data found",
+    UPDATE_SUCCESS:"Data successfully updated",
+    UPDATE_FAILURE:"Data update failure",
+    DUPLICATE_EMPID:"Duplicate employee id not allowed"
+ }
+
+ module.exports= responseData;
